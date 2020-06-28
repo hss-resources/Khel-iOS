b7eacec39be7c5156a64ccff4ea0c21e1f4977d8
Janak Shah <janak.shah@cuvva.com>
2020-06-28 12:17:30 +0100

    create Xcode project + update gitignore

---
+++ Khel/Khel/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Khel
+//
+//  Created by Janak Shah on 28/06/2020.
+//  Copyright © 2020 App Ktchn. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
